d263f618c13e4ca3dcd20bed46c9d959987b3066
marinlestylo <chil.tfm@gmail.com>
2020-01-20 11:26:34 +0100

    feat: add stopwatch, scramble and the scramble pop up

--- client/src/views/Play.vue
+++ client/src/views/Play.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <h1>Play</h1>
+    <h1>Pipou</h1>
+    <Scramble>
+      <ScramblePopUp />
+    </Scramble>
+    <Timer />
   </div>
 </template>
 
 <script>
+import ScramblePopUp from "@/components/Play/ScramblePopUp.vue"
+import Scramble from "@/components/Play/Scramble.vue"
+import Timer from "@/components/Play/Timer.vue"
 export default {
-}
+  components: {
+    ScramblePopUp,
+    Scramble,
+    Timer
+  }
+};
 </script>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -251,6 +251,7 @@ module.exports = {
       '4xl': '2.25rem',
       '5xl': '3rem',
       '6xl': '4rem',
+      '10xl': '7rem', 
     },
     fontWeight: {
       hairline: '100',
