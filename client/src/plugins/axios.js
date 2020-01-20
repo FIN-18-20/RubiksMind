545601caac094d4e9e1c6d4d806a1f12a6080e01
Lothindir <lothindir@gmail.com>
2020-01-20 09:54:03 +0100

    feat: Added adonis-ally and auth module
    Initialised auth module

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -4,7 +4,7 @@ export const axios = axiosHttp
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3333/' : ''
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost/api/' : ''
 
 export default {
   install(Vue) {
