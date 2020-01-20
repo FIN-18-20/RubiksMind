e6cb6f083995fbf0a56dd77f6de8eca5593521d5
Lazar <lazazr@gmail.com>
2020-01-20 20:37:17 +0100

    refactor: trying to make a smooth transition for times list

--- client/src/components/PlayInfos/PlayTimes.vue
+++ client/src/components/PlayInfos/PlayTimes.vue
@@ -8,17 +8,19 @@
         <div class="flex items-center justify-center mt-1 bg-blue-800 border border-blue-700 rounded-md" style="width:220px;height:280px;">
             <div class="overflow-y-scroll box-styles rounded-md" style="width:200px;height:260px;">
                 <div v-if="hasData">
-                  <div class="relative flex items-center justify-between" v-for="(time, index) in times" :key="time.try"
-                    :class="[index % 2 ? 'bg-blue-1000' : 'bg-blue-900' , index === 0  ? 'rounded-t-md' : '', index === times.length - 1 ? 'rounded-b-md' : '' , 'w-full h-8']">
-                    <div class="flex">
-                      <span class="w-6 ml-1 text-xs font-medium text-right text-blue-200">{{time.try}}.</span>
-                      <img src="@/assets/img/trophy.svg" class="ml-3" alt="trophy" v-if="time.personalBest">
+                  <transition-group name="times" tag="div">
+                    <div class="relative flex items-center justify-between" v-for="(time, index) in times.slice().reverse()" :key="time.try"
+                      :class="[index % 2 ? 'bg-blue-1000' : 'bg-blue-900' , index === 0  ? 'rounded-t-md' : '', index === times.length - 1 ? 'rounded-b-md' : '' , 'w-full h-8']">
+                      <div class="flex">
+                        <span class="w-6 ml-1 text-xs font-medium text-right text-blue-200">{{time.try}}.</span>
+                        <img src="@/assets/img/trophy.svg" class="ml-3" alt="trophy" v-if="time.personalBest">
+                      </div>
+                        <span class="text-sm italic font-medium text-center text-blue-300 absoluteElement" @click="addTime"> {{time.time}}</span>
+                      <div class="flex items-center justify-center w-6 h-6 mr-2 cursor-pointer" @click="removeTime(times.length - 1 - index)">
+                        <img src="@/assets/img/cross-delete-score.svg" class="w-auto h-3 cursor-pointer" alt="deleteScore">
+                      </div>
                     </div>
-                    <span class="absolute z-0 w-full text-sm italic font-medium text-center text-blue-300"> {{time.time}}</span>
-                    <div class="z-10 flex items-center justify-center w-6 h-6 mr-2 cursor-pointer">
-                      <img src="@/assets/img/cross-delete-score.svg" class="w-auto h-3 cursor-pointer" alt="deleteScore">
-                    </div>
-                  </div>
+                  </transition-group>
                 </div>
                 <div v-else class="flex flex-col items-center justify-center w-1/5 w-full h-full">
                     <img src="@/assets/img/timer.svg" alt="timer">
@@ -34,9 +36,31 @@
         box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
         scrollbar-width: none; 
     }
+
     ::-webkit-scrollbar {
         width: 0;
         height: 0;
+    }
+
+    .absoluteElement{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    
+    .fade-enter,
+    .fade-leave-to {
+      opacity: 0;
+    }
+
+    .fade-enter-active,
+    .fade-leave-active {
+      transition: 0.5s;
+    }
+
+    .times-move{
+      transition: transform 1s;
     }
 </style>
 
@@ -46,17 +70,25 @@ export default {
     return {
       hasData: true,
       times: [
-        { try: 100, time: '01:17.24', personalBest: false },
-        { try: 10, time: '59:59.10', personalBest: false },
-        { try: 9, time: '00:58.24', personalBest: false },
-        { try: 8, time: '00:03.47', personalBest: true },
-        { try: 4, time: '00:58.24', personalBest: false },
-        { try: 3, time: '00:58.24', personalBest: false },
-        { try: 2, time: '00:58.24', personalBest: false },
         { try: 1, time: '00:58.24', personalBest: false },
+        { try: 2, time: '00:58.24', personalBest: false },
+        { try: 3, time: '00:58.24', personalBest: false },
+        { try: 4, time: '00:58.24', personalBest: false },
+        { try: 8, time: '00:03.47', personalBest: true },
+        { try: 9, time: '00:58.24', personalBest: false },
+        { try: 10, time: '59:59.10', personalBest: false },
         { try: 30, time: '00:58.24', personalBest: false },
+        { try: 101, time: '01:17.24', personalBest: false },
       ]
     }
   },
+  methods: {
+    addTime() {
+      this.times.push({ try: this.times.length + 20, time: '00:00.00', personalBest: false })
+    },
+    removeTime(index) {
+      this.times.splice(index, 1)
+    }
+  }
 }
 </script>
