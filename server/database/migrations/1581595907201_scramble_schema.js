689b2957348a41858511a5df6cbb5d3c9c3ee4b7
Valentin Kaelin <kalanehd@gmail.com>
2020-02-13 13:18:35 +0100

    feat: create model/migration for scrambles

---
+++ server/database/migrations/1581595907201_scramble_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ScrambleSchema extends Schema {
+  up () {
+    this.create('scrambles', (table) => {
+      table.increments()
+      table.string('scramble', 72)
+    })
+  }
+
+  down () {
+    this.drop('scrambles')
+  }
+}
+
+module.exports = ScrambleSchema
