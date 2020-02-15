3806dc546feef262075e411a476c8bcb07ebdf85
Valentin Kaelin <kalanehd@gmail.com>
2020-02-15 18:26:51 +0100

    fix: display error message if there isnt any scramble in the database

--- server/app/Controllers/Http/CubeController.js
+++ server/app/Controllers/Http/CubeController.js
@@ -5,8 +5,15 @@ const Database = use('Database')
 class CubeController {
   async scramble({ response }) {
     const maxId = (await Database.from('scrambles').max('id as max').first()).max
-    const rand = Math.floor(Math.random() * maxId) + 1
 
+    // If there's no scramble generated in the database
+    if (!maxId) {
+      return response.json({
+        scramble: 'Please contact a website administrator to generate scrambles.'
+      })
+    }
+
+    const rand = Math.floor(Math.random() * maxId) + 1
     const scramble = await Database
       .select('scramble')
       .from('scrambles')
