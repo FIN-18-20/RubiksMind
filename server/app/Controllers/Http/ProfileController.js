c437fe678dc4e49fb9702e5fcd670c31bee36774
Valentin Kaelin <kalanehd@gmail.com>
2020-02-28 15:42:33 +0100

    feat: add rank of player in profiles

--- server/app/Controllers/Http/ProfileController.js
+++ server/app/Controllers/Http/ProfileController.js
@@ -14,15 +14,36 @@ class ProfileController {
       return response.noContent(null)
     }
 
+    const userId = user.id
+
     const times = await Database
       .select('id', 'time', 'user_id', 'created_at AS date')
       .from('times')
       .where('user_id', user.id)
       .orderBy('id', 'desc')
+      .limit(100)
+
+    const totalTimes = await Database
+      .from('times')
+      .where('user_id', userId)
+      .count('id as total')
+
+    const leaderboard = await Database
+      .select('times.user_id', 'times.time as time', 'times.created_at AS date')
+      .from('times')
+      .min('times.time as time')
+      .groupBy('times.user_id')
+      .orderBy('time', 'asc')
+      .orderBy('times.created_at', 'asc')
+
+    let rank = leaderboard.findIndex(player => player.user_id === userId)
+    rank === -1 ? 'Not in leaderboard' : rank++
 
     return response.json({
-      user,
+      rank,
       times,
+      totalTimes: totalTimes[0].total,
+      user,
     })
   }
 }
