b9b164c76ccb613932881421a7452979e16a3296
marinlestylo <chil.tfm@gmail.com>
2020-03-02 10:59:16 +0100

    fix: design cube are now responsive

--- client/src/views/Leaderboard.vue
+++ client/src/views/Leaderboard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="w-full">
     <TitleAndCup class="mb-2" />
     <TimeList class="relative z-20" />
-    <img src="@/assets/img/leaderboard-cube.svg" alt="leftcube" class="absolute leftCube z-10 hidden sm:block">
-    <img src="@/assets/img/leaderboard-cube.svg" alt="rightcube" class="absolute rightCube z-10 hidden sm:block">
+    <img src="@/assets/img/leaderboard-cube.svg" alt="leftcube" class="absolute leftCube z-10 select-none hidden sm:block">
+    <img src="@/assets/img/leaderboard-cube.svg" alt="rightcube" class="absolute rightCube z-10 select-none hidden sm:block">
   </div>
 </template>
 
@@ -19,14 +19,44 @@ export default {
 </script>
 
 <style scoped>
+
+@screen md{
   .leftCube{
-    top: 25%;
-    left: 21%;
+    top: 35%;
+    left: -13%;
   }
 
   .rightCube{
-    top: 25%;
-    right: 21%;
+    top: 35%;
+    right: -13%;
     transform: rotate(180deg)
   }
+}
+
+@screen lg{
+  .leftCube{
+    top: 35%;
+    left: 3%;
+  }
+
+  .rightCube{
+    top: 35%;
+    right: 3%;
+    transform: rotate(180deg)
+  }
+}
+
+@screen xl{
+  .leftCube{
+    top: 35%;
+    left: 12%;
+  }
+
+  .rightCube{
+    top: 35%;
+    right: 12%;
+    transform: rotate(180deg)
+  }
+}
+  
 </style>
